523f01db5342273dac4e635cc8c816faf72371c9
Kyohei Hamada <kyohei.hamada@gmail.com>
2013-02-02 15:27:38 +0900

    initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitchat::Application.config.secret_token = '12b72b0214d22a17e4984308abf1d1ec0da637d34eb820d1d36a616c58c0bd7e2f77f8ab690b45bec99f33e3434fbd09b1215050e418fa1bab5e32f63e379f54'
