61a9d4840ea0cd8d29ede8c22be1f7b5449ed4d0
Kyohei Hamada <kyohei.hamada@gmail.com>
2013-02-03 00:04:49 +0900

    access token を取得する部分(途中)

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,15 +1,34 @@
+# coding: utf-8
+require 'net/https'
 class LoginController < ApplicationController
 
   # GET /login
   # GET /login.json
   def index
-    @client = Octokit::Client.new(:login => "me",  :oauth_token => "oauth2token")
-    # render :text => @client
+    code = params[:code].to_s
+    if code.empty?
+      respond_to do |format|
+        format.html { redirect_to '/' }
+      end
+    end
 
-    # client.follow("sferik")
-    # respond_to do |format|
-      # format.html # index.html.erb
-      # format.json { render json: @client }
-    # end
+    # access_token を取得するため，postリクエストする
+    uri = ['https://', Settings.github.host, Settings.github.access_token_path].join
+    post_params = {
+      :client_id     => Settings.github.client_id,
+      :client_secret => Settings.github.client_secret,
+      :code          => code,
+    }
+    p URI.parse(uri)
+    p post_params
+    @post = Net::HTTP.post_form(URI.parse(uri), post_params)
+    # render :text => post
+  end
+
+  def access_token
+    text  = 'idパラメータ: '
+    text += params[:id] ? params[:id] : 'undefined'
+    render :text => text
+    # @client = Octokit::Client.new(:login => "me",  :oauth_token => "oauth2token")
   end
 end
